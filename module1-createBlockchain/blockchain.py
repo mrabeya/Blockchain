421b21734a305d43c6422fde4166e41ccfa64b40
mrabeya <maksuda.rabeya@gmail.com>
2025-09-17 09:04:00 -0400

    Update blockchain.py
    is_valid() function is added

--- module1-createBlockchain/blockchain.py
+++ module1-createBlockchain/blockchain.py
@@ -98,5 +98,15 @@ def get_chain():
                 'length' : len(blockchain.chain)}
     return jsonify(response), 200
 
+#checking if the Blockchain is valid
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message' : 'All good. The blockchain is valid.'}
+    else:
+        response = {'message' : 'We have a problem. The blockchain is not valid.'}
+    return jsonify(response), 200
+
 # Running the app
 app.run(host = '0.0.0.0', port = 5001)
